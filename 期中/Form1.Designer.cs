cdcff4b1e023c6debbda9dd42224bc657730953f
yoyolien <59311189+yoyolien@users.noreply.github.com>
2023-05-23 18:51:39 +0800

    Add files via upload

---
+++ 期中/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace midterm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.point = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.point)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 200;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // point
+            // 
+            this.point.Location = new System.Drawing.Point(0, 0);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(125, 62);
+            this.point.TabIndex = 0;
+            this.point.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.point);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.point)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox point;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 期中/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace midterm
+{
+    public partial class Form1 : Form
+    {
+
+        Random r = new Random();
+        int q;
+        int disx;
+        int disy;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+            PictureBox a = new PictureBox();
+            this.Controls.Add(a);
+            a.Name = "a";
+            a.Size = new Size(20, 20);
+            a.BackColor = Color.FromArgb(r.Next(100, 255), r.Next(100, 255), r.Next(100, 255));
+            cir(a);
+            a.Left = r.Next(0, 1000);
+            a.Top = r.Next(0, 1000);
+            if (timer1.Interval >10)
+            timer1.Interval -= 10;
+            q++;
+            label1.Text = q.ToString();
+        }
+        void cir(PictureBox pic)
+        {
+            GraphicsPath g = new GraphicsPath();
+            g.AddEllipse(pic.ClientRectangle);
+            Region region = new Region(g);
+            pic.Region = region;
+            g.Dispose();
+            region.Dispose();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            point.Size = new Size(30, 30);
+            point.BackColor = Color.Black;
+            this.ClientSize = new Size(1000, 1000);
+            cir(point);
+            point.Left = 500;
+            point.Top = 500;
+            timer2.Interval = 1000;
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            point.Left += (-point.Left - 15 + Cursor.Position.X) / 20;
+            point.Top += (-point.Top - 15 + Cursor.Position.Y) / 20;
+            foreach (Control picbox in this.Controls)
+            {
+                {
+                    if (picbox.Name == "a")
+                    {
+                        picbox.Left += (point.Left + 5 - picbox.Left) / 12;
+                        picbox.Top += (point.Top + 5 - picbox.Top) / 12;
+                        disx = point.Left + 5 - picbox.Left;
+                        disy = point.Top + 5 - picbox.Top;
+                        if (disx < 0) disx = -disx;
+                        if (disy < 0) disy = -disy;
+                        if (disx * disx + disy * disy < 625) {
+                            timer1.Enabled = false;
+                            timer2.Enabled = false;
+                            MessageBox.Show("dead");
+                            restart();
+                        }
+                    }
+                }
+            }
+        }
+        void restart()
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            point.Left = 500;
+            point.Top = 500;
+            List<Control> del = new List<Control>();
+            foreach (Control d in this.Controls)
+            {
+                if (d.Name == "a")
+                {
+                    del.Add(d as PictureBox); 
+                }
+            }
+            foreach (Control d in del)
+            {
+                this.Controls.Remove(d);
+            }
+            timer2.Interval = 1000;
+            q = 0;
+        }
+    }
+}
